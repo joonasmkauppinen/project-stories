3faf693457d6708399791cdb8f32f0ac531eaca3
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2022-02-16 20:27:01 +0200

    test(addNewCard): refactor test to use new `generateCardsSlice` utility function

--- packages/store-zustand/src/actions/addNewCard/__tests__/addNewCard.spec.ts
+++ packages/store-zustand/src/actions/addNewCard/__tests__/addNewCard.spec.ts
@@ -1,70 +1,36 @@
-import { addNewCard } from '../addNewCard';
-
 import { AppState, TestCardId } from '../../../types';
-import { generateCard } from '../../../generators';
-import { testInitialState } from '../../__test-utils__/testInitialState';
+import { defaultInitialState } from '../../__test-utils__/defaultInitialState/defaultInitialState';
 import { useStore } from '../../../store/zustandStore';
 
-const testCardId1: TestCardId = 'test_card_id_0';
-const testCardId2: TestCardId = 'test_card_id_1';
+import { generateCardsSlice } from '../../__test-utils__';
 
-const emptyCard = generateCard({
-  sortOrderIndex: 0,
-  testOverrides: { id: testCardId1 },
-}).idWithData;
+import { addNewCard } from '../addNewCard';
 
-const testStateWithEmptyCard: AppState = {
-  ...testInitialState,
-  cards: {
-    ...emptyCard,
-  },
-};
-
-beforeEach(() => {
-  useStore.setState(() => testStateWithEmptyCard);
-});
-
-describe('Action - addNewCard', () => {
+describe('Action - addNewCard()', () => {
   test('adds new card object correctly', () => {
-    const initialCard = useStore.getState().cards[testCardId1];
-    expect(initialCard.name).toEqual('Card 1');
-    expect(initialCard.sortOrderIndex).toEqual(0);
-    expect(initialCard.state).toEqual('idle');
+    const cardId1: TestCardId = 'test_card_id_0';
+    const cardId2: TestCardId = 'test_card_id_1';
 
-    addNewCard({ testOverrides: { id: testCardId2 } });
+    const stateWithOneEmptyCard: AppState = {
+      ...defaultInitialState,
+      cards: generateCardsSlice([{ id: cardId1 }]),
+    };
 
-    const newCard = useStore.getState().cards[testCardId2];
+    useStore.setState(() => stateWithOneEmptyCard);
+
+    const firstCard = useStore.getState().cards[cardId1];
+    expect(firstCard.name).toEqual('Card 1');
+    expect(firstCard.sortOrderIndex).toEqual(0);
+    expect(firstCard.state).toEqual('idle');
+
+    addNewCard({ testOverrides: { id: cardId2 } });
+
+    const newCard = useStore.getState().cards[cardId2];
     expect(newCard.name).toEqual('Card 2');
     expect(newCard.sortOrderIndex).toEqual(1);
     expect(newCard.state).toEqual('idle');
 
-    expect(useStore.getState().cards).toMatchInlineSnapshot(`
-      Object {
-        "test_card_id_0": Object {
-          "autoAdvance": false,
-          "duration": 0,
-          "layers": Object {},
-          "name": "Card 1",
-          "screenPosition": Object {
-            "x": NaN,
-            "y": NaN,
-          },
-          "sortOrderIndex": 0,
-          "state": "idle",
-        },
-        "test_card_id_1": Object {
-          "autoAdvance": false,
-          "duration": 0,
-          "layers": Object {},
-          "name": "Card 2",
-          "screenPosition": Object {
-            "x": NaN,
-            "y": NaN,
-          },
-          "sortOrderIndex": 1,
-          "state": "idle",
-        },
-      }
-    `);
+    const cardValues = Object.values(useStore.getState().cards);
+    expect(cardValues.length).toBe(2);
   });
 });
