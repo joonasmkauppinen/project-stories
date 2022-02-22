ea0721df484cc3a23846ac3ac9048eadf81eef95
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2022-02-22 10:38:26 +0200

    refactor(Storyboard): use new state slice 'userInteraction'

--- packages/ui-storyboard-renderer/src/components/Storyboard/Storyboard.tsx
+++ packages/ui-storyboard-renderer/src/components/Storyboard/Storyboard.tsx
@@ -3,6 +3,7 @@ import {
   Cards,
   LayerActionsProp,
   SelectedLayer,
+  UserInteraction,
 } from '@joonasmkauppinen/project-stories/store-zustand';
 
 import {
@@ -19,7 +20,7 @@ import { useCallback, useMemo } from 'react';
 export interface StoryboardProps extends LayerActionsProp {
   cards: Cards;
   selectedLayers: SelectedLayer[];
-  isDragging: boolean;
+  userInteraction: UserInteraction;
 }
 
 const StyledStoryboard = styled.div({
@@ -41,7 +42,7 @@ export const Storyboard = ({
   cards,
   actions,
   selectedLayers,
-  isDragging,
+  userInteraction,
 }: StoryboardProps) => {
   // Padding needed to bring the first card to the middle of the screen.
   const PADDING_HORIZONTAL = useMemo(() => {
@@ -79,7 +80,7 @@ export const Storyboard = ({
       <Selection
         cards={cards}
         selectedLayers={selectedLayers}
-        isDragging={isDragging}
+        isDragging={userInteraction.isDragging}
       />
     </StyledStoryboard>
   );
