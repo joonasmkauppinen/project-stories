93f6816322557529cb4bc4aec04d66db8a748011
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2022-02-25 12:54:57 +0200

    fix(setTextLayerValue): add a guard check to only try to update text layer values

--- packages/store-zustand/src/actions/setTextLayerValue/setTextLayerValue.ts
+++ packages/store-zustand/src/actions/setTextLayerValue/setTextLayerValue.ts
@@ -1,7 +1,8 @@
 import produce from 'immer';
 
-import { AppState, ID } from '../..';
+import { AppState, ID, TextLayerType } from '../../types';
 import { useStore } from '../../store/zustandStore';
+import { isTextLayer } from '../../guards/isTextLayer';
 
 export interface SetTextLayerValuePayload {
   value: string;
@@ -18,6 +19,16 @@ export const setTextLayerValue: SetTextLayerValue = ({
 }) =>
   useStore.setState(
     produce<AppState>((draft) => {
-      draft.cards[cardId].layers[layerId].value = value;
+      const state = useStore.getState();
+      const layerToUpdate = state.cards[cardId].layers[layerId];
+
+      if (isTextLayer(layerToUpdate)) {
+        (draft.cards[cardId].layers[layerId] as TextLayerType).value = value;
+      } else {
+        console.warn(
+          'Attempted to call action "setTextLayerValue()" on layer type: ',
+          layerToUpdate.type
+        );
+      }
     })
   );
